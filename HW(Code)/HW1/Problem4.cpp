3ee71e5030c38d184b2a43d587ac4f4b037b0cc6
Krish Shah <2003kshah@gmail.com>
2023-09-07 21:22:47 -0400

    pushing problem4 - hw1

---
+++ HW(Code)/HW1/Problem4.cpp
@@ -0,0 +1,49 @@
+// Krish Shah, U75802512, kshah26@bu.edu
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool legalBrackets(const string& line) {
+    stack<char> bracketStack;
+
+    for (char ch : line) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            bracketStack.push(ch);
+        } else if (ch == ')' || ch == '}' || ch == ']') {
+            if (bracketStack.empty()) {
+                return false;
+            }
+
+            char top = bracketStack.top();
+            bracketStack.pop();
+
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+    return bracketStack.empty(); 
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input_string>" << endl;
+        return 1;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        if (legalBrackets(argv[i])) {
+            cout << "legal" << endl;
+        } else {
+            cout << "illegal" << endl;
+        }
+    }
+
+    return 0;
+}
